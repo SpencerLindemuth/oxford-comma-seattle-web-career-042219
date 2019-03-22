cdf8814ab9f51162832f2c7bda429a4abe215931
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 17:54:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  base = ""
+  if array.length <= 2
+    return array.join(" and ")
+  else
+    array.each_index do |index|
+      if (index + 1) < array.length
+        base += "#{array[index]}, "
+      else
+        base += "and #{array[index]}"
+      end
+    end
+  end
+  return base
 end
