b78b4135e0de5c1a69eb10c36cf1fe5d37d2ef84
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 17:52:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-  array.join(" and ")
+  base = ""
+  if array.length <= 2
+    array.join(" and ")
+  else
+    array.each_index do |index|
+      if (index + 1) < array.length
+        base += "#{array[index]}, "
+      else
+        base += "and #{array[index]}"
+      end
+    end
+    return base
 end
